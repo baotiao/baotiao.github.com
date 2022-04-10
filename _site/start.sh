ee7f2c5901cc5869ed6002ac317050b978fcd312
zongzhi.czz <zongzhi.czz@alibaba-inc.com>
2022-04-11 03:07:19 +0800

    add

---
+++ _site/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+para=$#;
+
+if [ $para != 1 ]; then
+    echo "we just need one parameter, line or local";
+    exit 1;
+fi
+
+place=$1;
+
+if [ $place == "local" ]; then
+    sed '3s/\(url:\ http:\/\/\).*/\1localhost:4000/g' _config.yml >> _config.yml.tt
+    mv _config.yml.tt _config.yml
+elif [ $place == "line" ]; then
+    sed '3s/\(url:\ http:\/\/\).*/\1baotiao.github.io/g' _config.yml >> _config.yml.tt
+    mv _config.yml.tt _config.yml;
+else
+    echo "we just need line or local";
+    exit 2;
+fi
+bundle exec jekyll serve
+# jekyll server --watch
